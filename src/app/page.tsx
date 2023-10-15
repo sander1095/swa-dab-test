3e3437fd582a81436c2bad8bf8d999f9c6b8aabc
Sander ten Brinke <s.tenbrinke2@gmail.com>
2023-10-15 15:00:54 +0200

    Configure CORS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 async function getData() {
-
-  // let host = process.env.NODE_ENV === 'production' ? 'http://localhost:4280/' : 'http://localhost:4280/'; // This is where the database thing runs
-  let res = await fetch(`/data-api/rest/Person`);
+  let host = process.env.NODE_ENV === 'production' ? 'https://calm-hill-09bbb1f03.3.azurestaticapps.net/' : 'http://localhost:4280/'; // This is where the database thing runs
+  let res = await fetch(`${host}data-api/rest/Person`);
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
