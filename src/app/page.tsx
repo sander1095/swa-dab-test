fe83a9ec8d5c9b719a7477c7b3118c0c3077d68f
Sander ten Brinke <s.tenbrinke2@gmail.com>
2023-10-15 15:14:35 +0200

    Final attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 async function getData() {
+  let host = 'https://calm-hill-09bbb1f03.3.azurestaticapps.net/'
+  // let host = process.env.NODE_ENV === 'production' ? 'https://calm-hill-09bbb1f03.3.azurestaticapps.net/' : 'http://localhost:4280/'; // This is where the database thing runs
+  let res = await fetch(`${host}data-api/rest/Person`);
 
-  if (new Date().getMinutes() > 10) {
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
 
-    let host = 'https://calm-hill-09bbb1f03.3.azurestaticapps.net/'
-    // let host = process.env.NODE_ENV === 'production' ? 'https://calm-hill-09bbb1f03.3.azurestaticapps.net/' : 'http://localhost:4280/'; // This is where the database thing runs
-    let res = await fetch(`${host}data-api/rest/Person`);
-
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data')
-    }
-
-    let text = await res.text();
-    return text;
-  } else { return "NOT LOADING DATABASE YET" }
+  let text = await res.text();
+  return text;
 }
 
 export default async function Page() {
